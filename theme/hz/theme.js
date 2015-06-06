cb0c785a0dc998faf7ff4492914195e3bb1a1f8c
Michael Wolf <pharesim@nhzcrypto.org>
2015-06-06 04:38:13 +0200

    theme changes (notefield hidden in pf version)

--- theme/hz/theme.js
+++ theme/hz/theme.js
@@ -1,2 +1,9 @@
-$("#container").height(504).width(800).css("background",'url("theme/hz/img/hzbg.jpg") no-repeat').css('padding-top','120px');
-$("#container fieldset, legend").css("background","rgba(255,255,255,0.6)");
+$("#notefield").show();
+
+$("#container").
+	height(504).
+	width(800).
+	css("background",'url("theme/hz/img/hzbg.jpg") no-repeat').css('padding-top','120px');
+
+$("#container fieldset, legend").
+	css("background","rgba(255,255,255,0.6)");
--- theme/hz_pf/theme.js
+++ theme/hz_pf/theme.js
@@ -1,2 +1,10 @@
-$("#container").height(504).width(800).css("background",'url("theme/hz_pf/img/hzbg.jpg") no-repeat').css('padding-top','120px');
-$("#container fieldset, legend").css("background","rgba(255,255,255,0.6)");
+$("#notefield").hide();
+
+$("#container").
+	height(504).
+	width(800).
+	css("background",'url("theme/hz_pf/img/hzbg.jpg") no-repeat').
+	css('padding-top','140px');
+
+$("#container fieldset, legend").
+	css("background","rgba(255,255,255,0.6)");
