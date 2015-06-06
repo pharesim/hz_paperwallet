fac6463d003a2311bdd3ef4c085b1a441fd73ccf
Michael Wolf <pharesim@nhzcrypto.org>
2015-06-06 04:53:33 +0200

    theme printer friendly with note

---
+++ theme/hz_pf_note/theme.js
@@ -0,0 +1,15 @@
+$("#container").
+	height(504).
+	width(800).
+	css("background",'url("theme/hz_pf_note/img/hzbg.jpg") no-repeat')
+	.css('padding-top','120px');
+
+$("#container fieldset, legend").
+	css("background","rgba(255,255,255,0.6)");
+
+$("#notefield").show();
+$("#notefield input").
+	css("background-color","transparent").
+	css("border","0px solid").
+	css("outline","none").
+	css("width","100%");
