db16e7c3f4007c8e8353768540f980146df04f4f
Michael Wolf <pharesim@nhzcrypto.org>
2015-06-06 02:51:04 +0200

    printer friendly theme

---
+++ theme/hz_pf/theme.js
@@ -0,0 +1,2 @@
+$("#container").height(504).width(800).css("background",'url("theme/hz_pf/img/hzbg.jpg") no-repeat').css('padding-top','120px');
+$("#container fieldset, legend").css("background","rgba(255,255,255,0.6)");
